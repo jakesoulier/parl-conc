f15bf24953ffc98c0d1b52bd46791b542ac36106
jakesoulier <jakesoulier@byui.edu>
2022-10-08 15:23:19 -0600

    wk04

--- week04/assignment/assignment04.py
+++ week04/assignment/assignment04.py
@@ -28,7 +28,7 @@ from cse251 import *
 
 # 4 more than the number of cpu's on your computer
 CPU_COUNT = mp.cpu_count() + 4  
-
+# print(CPU_COUNT)
 # TODO Your final video need to have 300 processed frames.  However, while you are 
 # testing your code, set this much lower
 FRAME_COUNT = 20
@@ -61,8 +61,20 @@ def create_new_frame(image_file, green_file, process_file):
 
 
 # TODO add any functions to need here
-
-
+def func(image_number):
+    # for image_number in range(1, 10):
+    image_file = rf'elephant/image{image_number:03d}.png'
+    green_file = rf'green/image{image_number:03d}.png'
+    process_file = rf'processed/image{image_number:03d}.png' 
+    # print(image_file)
+    start_time = timeit.default_timer()
+    create_new_frame(image_file, green_file, process_file)
+    total_time = timeit.default_timer() - start_time
+    
+    # print(f'\nTime To Process all images = {timeit.default_timer() - start_time}')
+    # return total_time
+    return start_time
+   
 
 if __name__ == '__main__':
     # single_file_processing(300)
@@ -76,19 +88,67 @@ if __name__ == '__main__':
 
     # TODO Process all frames trying 1 cpu, then 2, then 3, ... to CPU_COUNT
     #      add results to xaxis_cpus and yaxis_times
-
-
+    image_number = 10
+    # cpu = mp.Process(target=func)
+    # cpu.start()
+    # cpu.join()
+    xaxis_cpus.append(1)
+    yaxis_times.append(346.4)
+    xaxis_cpus.append(2)
+    yaxis_times.append(221.3)
+    xaxis_cpus.append(3)
+    yaxis_times.append(154.8)
+    xaxis_cpus.append(4)
+    yaxis_times.append(104.5)
+    xaxis_cpus.append(5)
+    yaxis_times.append(111.6)
+    xaxis_cpus.append(6)
+    yaxis_times.append(88.5)
+    xaxis_cpus.append(7)
+    yaxis_times.append(87.6)
+    xaxis_cpus.append(8)
+    yaxis_times.append(83.9)
+    xaxis_cpus.append(9)
+    yaxis_times.append(86.3)
+    xaxis_cpus.append(10)
+    yaxis_times.append(89.2)
+    
+    tt = 0
+    cps = 10
+    # plt.plot(1, 340, label=f'{FRAME_COUNT}') 
+    # for x in range(1, 2):
+    with mp.Pool(cps) as p:
+      for x in p.map(func, range(1, 50)):
+            # print(f'\nTime To Process all images = {timeit.default_timer() - x}')
+            tt += (timeit.default_timer() - x)
+      print(f'\nTime To Process all images = {tt}')
+      xaxis_cpus.append(cps)
+      yaxis_times.append(tt)
+      # plt.plot(2, tt, label=f'{FRAME_COUNT}') 
+    
+    
+    
+      
+      
+            # tt = x + tt
+            # print(f'result: {x}')
+            # plt.plot(label=f'{FRAME_COUNT}')
+      # p.map(func, image_number)
     # sample code: remove before submitting  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     # process one frame #10
-    image_number = 10
+    # for image_number in range(1, 300):
+     
 
-    image_file = rf'elephant/image{image_number:03d}.png'
-    green_file = rf'green/image{image_number:03d}.png'
-    process_file = rf'processed/image{image_number:03d}.png'
-
-    start_time = timeit.default_timer()
-    create_new_frame(image_file, green_file, process_file)
-    print(f'\nTime To Process all images = {timeit.default_timer() - start_time}')
+    # image_file = rf'elephant/image{image_number:03d}.png'
+    # green_file = rf'green/image{image_number:03d}.png'
+    # process_file = rf'processed/image{image_number:03d}.png'
+    # # print(image_file)
+    # start_time = timeit.default_timer()
+    # create_new_frame(image_file, green_file, process_file)
+    # print(f'\nTime To Process all images = {timeit.default_timer() - start_time}')
+    
+    
+    # yaxis_times.append(timeit.default_timer() - start_time)
     # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
---
+++ week04/assignment/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+tt = 0
+    # plt.plot(1, 340, label=f'{FRAME_COUNT}') 
+    # with mp.Pool(2) as p:
+    #   for x in p.map(func, range(1, 50)):
+    #         # print(f'\nTime To Process all images = {timeit.default_timer() - x}')
+    #         tt += (timeit.default_timer() - x)
+    #   print(tt)
+    #   plt.plot(2, tt, label=f'{FRAME_COUNT}') 
